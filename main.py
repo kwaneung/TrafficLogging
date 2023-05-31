46ed6eb568732aed82ef4baa90771e66ffb4cd10
kwaneung <kwaneung.kim@outlook.com>
2023-05-31 22:53:03 +0900

    생성

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import datetime as dt
+import time
+import pandas as pd
+
+if __name__ == '__main__':
+    print('init params')
+
+    # init DataFrame
+    df = pd.DataFrame(columns=['Time', 'URL', 'Return'])
+
+    # init count
+    count = 0
+
+    # init URL
+    target_url = 'https://www.youtube.com'
+
+    print('ready for logging')
+
+    while True:
+        # init date
+        date = dt.datetime.now()
+
+        time.sleep(1)
+        # call target_url and append DataFrame
+        df.loc[count] = [dt.datetime.now(), target_url, requests.get(target_url)]
+
+        count += 1
+
+        if date.hour == 23 and date.minute == 59:
+            if dt.datetime.now().month < 10:
+                filename = str(date.year) + '-0' + str(date.month) + '-' + str(date.day) + ".csv"
+            else:
+                filename = str(date.year) + '-' + str(date.month) + '-' + str(date.day) + ".csv"
+
+            df.to_csv(filename, mode='w')
